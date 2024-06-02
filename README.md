11f9860876511e24f409ddc35df1514904546ff7
Lucas Åström <lucas.astrom00@gmail.com>
2024-06-02 15:45:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# torrent-poc
+
+A sans-io proof-of-concept implementation of the torrent protocol,
+implemented in Rust as a programming challenge for recruitment purposes.
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#![warn(clippy::unwrap_used)]
+//#![warn(missing_docs)]
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
